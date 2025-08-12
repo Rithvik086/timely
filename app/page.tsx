57cbaf50b57bf87b42eb41a92c892e250696658b
Rithvik086 <145756002+Rithvik086@users.noreply.github.com>
2025-08-12 16:09:02 +0530

    baisc login implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import { Button } from "antd";
 export default function Home() {
   return (
     <div className={styles.page}>
@@ -44,6 +44,9 @@ export default function Home() {
           >
             Read our docs
           </a>
+
+          <a href="/auth/login">Login</a>
+          <a href="/auth/logout">Logout</a>
         </div>
       </main>
       <footer className={styles.footer}>
